418fb448cc2fc44fa09ab41743dad36fe5365036
AliSiddique <alisiddique10@hotmail.com>
2024-02-03 00:16:42 +0000

    Initial Commit

---
+++ planetapp/planetappApp.swift
@@ -0,0 +1,17 @@
+//
+//  planetappApp.swift
+//  planetapp
+//
+//  Created by Ali Siddique on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct planetappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
